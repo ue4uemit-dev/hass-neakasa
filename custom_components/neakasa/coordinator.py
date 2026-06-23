9cbbac816a8b52ff1c2bae1edfa7312d5956be78
ue4uemit-dev <ue4.uemit@gmail.com>
2026-06-23 18:08:53 +0200

    Update coordinator.py

--- custom_components/neakasa/coordinator.py
+++ custom_components/neakasa/coordinator.py
@@ -94,6 +94,7 @@ class NeakasaCoordinator(DataUpdateCoordinator):
         try:
             await self.api.connect(self.username, self.password)
             devicedata = await self.api.getDeviceProperties(self.deviceid)
+            _LOGGER.info(devicedata)
             return NeakasaAPIData(
                 binFullWaitReset=devicedata['binFullWaitReset']['value'] == 1, #-> Abfalleimer voll
                 youngCatMode=devicedata['youngCatMode']['value'] == 1, #-> Kätzchen Modus
